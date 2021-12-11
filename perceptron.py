cb005f7cf89ebaee8d98c161b85342e917747434
Reisel <reiselmillan@gmail.com>
2021-12-11 14:56:29 -0500

    changed name

--- perceptron.py
+++ perceptron.py
@@ -1,3 +1,4 @@
+from matplotlib import markers
 import numpy as np
 
 class Perceptron(object):
@@ -82,5 +83,39 @@ plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('NUmber of misclasifications')
 plt.show()
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # setup marker generator
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lighgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1,  X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1,  X[:, 1].max() + 1
+
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                            np.arange(x2_min, x2_max, resolution))
+
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, c1 in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == c1, 0], y=X[y == c1, 1], 
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=c1)
+
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+plt.show()
+
 
 
